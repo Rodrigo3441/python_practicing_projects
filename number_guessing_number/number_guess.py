df1e0f97ca85be1c85114adafa40a2fb9630a71c
Rodrigo <119820531+Rodrigo3441@users.noreply.github.com>
2026-01-27 16:31:08 -0300

    Add files via upload
    a game where the computer generates a random number and the user has to find what number it is. 
    
    I used my logical skill for doing it, and I think the result is very nice
    i used:
    random library;
    try---except, for validating some errors;
    while loops;
    if statements.

---
+++ number_guessing_number/number_guess.py
@@ -0,0 +1,46 @@
+# Number Guessing Game
+# User chooses the number-range
+# Computer picks a random number between 0 and the number chosen by user
+# User guesses until correct
+# hints: too high / too low
+import random
+
+def only_numbers():
+    print('Enter only integer numbers!')
+    return 0
+
+while True:
+    try:
+        number_range = int(input('Type the range you want the numbers to be generated (at least 10): '))
+    except ValueError:
+        only_numbers()
+    else:
+        if number_range < 10:
+            print('use only integer numbers higher than 10')
+        else:
+            break
+
+computer_number = int(random.random() * number_range)
+user_number = None
+attempts = 0
+print(f'Number guessing game, range: 0 - {number_range-1}')
+print('=' * 40)
+
+while user_number != computer_number:
+    try:
+        user_number = int(input('Enter a number: '))
+    except ValueError:
+        only_numbers()
+    else: 
+        if user_number >= 0 and user_number < number_range:
+            attempts += 1
+            if user_number == computer_number:
+                print('You found the number! congratulations')
+                print(f'Number of attempts: {attempts}')
+                break
+            elif user_number < computer_number:
+                print('Too low, try again')
+            else:
+                print('Too high, try again')
+        else:
+            print(f'Do not enter a number outsite the range! (0 - {number_range-1})')
