02587c871b50b352653a401e504fe65d2ae50341
Rodrigo <119820531+Rodrigo3441@users.noreply.github.com>
2026-03-19 18:26:57 -0300

    Delete simple_calculator directory

--- simple_calculator/console_treat.py
+++
@@ -1,3 +0,0 @@
-import os
-clear = lambda: os.system('cls') # function to clear the console up
-pause = lambda: os.system('pause') # function to pause the console
--- simple_calculator/save_in_history.py
+++
@@ -1,3 +0,0 @@
-def save_in_history(filename, number1, operator, number2, result):
-    with open(filename, 'a') as f:
-        f.write(f'{number1},{operator},{number2},{result}\n')
