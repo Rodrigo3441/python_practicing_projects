8ef86864892b00b6ab81c30c9ab8264d71c12726
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-14 19:42:23 -0300

    refactor

--- api_exercises/exercise_02/api/JsonPlaceholderAPI.py
+++ api_exercises/exercise_02/api/JsonPlaceholderAPI.py
@@ -11,18 +11,18 @@ class JsonPlaceholderAPI:
 
     def get_posts(self) -> requests.Response:
         return requests.get(f'{self.base_url}/{self.data_sources[0]}', timeout=5).json()
-
+         
     def get_comments(self) -> requests.Response:
-            return requests.get(f'{self.base_url}/{self.data_sources[1]}', timeout=5).json()
+        return requests.get(f'{self.base_url}/{self.data_sources[1]}', timeout=5).json()
 
     def get_albums(self) -> requests.Response:
-            return requests.get(f'{self.base_url}/{self.data_sources[2]}', timeout=5).json()
+        return requests.get(f'{self.base_url}/{self.data_sources[2]}', timeout=5).json()
 
     def get_photos(self) -> requests.Response:
-            return requests.get(f'{self.base_url}/{self.data_sources[3]}', timeout=5).json()
+        return requests.get(f'{self.base_url}/{self.data_sources[3]}', timeout=5).json()
 
     def get_todos(self) -> requests.Response:
-            return requests.get(f'{self.base_url}/{self.data_sources[4]}', timeout=5).json()
+        return requests.get(f'{self.base_url}/{self.data_sources[4]}', timeout=5).json()
 
     def get_users(self) -> requests.Response:
-            return requests.get(f'{self.base_url}/{self.data_sources[5]}', timeout=5).json()
+        return requests.get(f'{self.base_url}/{self.data_sources[5]}', timeout=5).json()
