8b3774ec772e675bc7c99e1a530450f13e8c6a97
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-14 19:44:56 -0300

    Refactor main function to remove unused pandas import and streamline data processing

--- api_exercises/exercise_02/main.py
+++ api_exercises/exercise_02/main.py
@@ -4,17 +4,16 @@
 
 from api import JsonPlaceholderAPI as api
 from src import extract
-import pandas as pd
+from src import transform
+from src import load
 
 def main():
     base_url = 'https://jsonplaceholder.typicode.com'
     data_sources = ['posts', 'comments', 'albums', 'photos', 'todos', 'users']
 
     raw_data = extract.run_extraction(base_url, data_sources)
-
-    print(pd.DataFrame(raw_data[5]).dtypes)  # Display the data types of the 'users' DataFrame
-    
-
+    cleaned_data: list = transform.run_transformations(raw_data)
+    load.load_data(cleaned_data)
 
 if __name__ == '__main__':
     main()
