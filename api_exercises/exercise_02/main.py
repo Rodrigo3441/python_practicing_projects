9cd50bf9c5b122e01695c445294d9efce618f706
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-14 16:38:51 -0300

    Refactor main.py to streamline data extraction using extract module and remove redundant code

--- api_exercises/exercise_02/main.py
+++ api_exercises/exercise_02/main.py
@@ -3,42 +3,17 @@
 # JSONPlaceholder API and convert the returned JSON data into DataFrames.
 
 from api import JsonPlaceholderAPI as api
+from src import extract
 import pandas as pd
-
-from api import JsonPlaceholderAPI as api
-import pandas as pd
-
-def print_separator():
-    print("=================================================")
 
 def main():
-    url = 'https://jsonplaceholder.typicode.com/posts/'
+    base_url = 'https://jsonplaceholder.typicode.com'
+    data_sources = ['posts', 'comments', 'albums', 'photos', 'todos', 'users']
 
-    json_api = api.JsonPlaceholderAPI(url)
-    response = json_api.get_posts()
-    response_dict = response.json()
+    raw_data = extract.run_extraction(base_url, data_sources)
 
-    print_separator()
-    print(f'THE RESPONSE TYPE: {type(response)}')
-    # print(f'The response content: {response_dict}')
-
-    response_df = pd.DataFrame(response_dict)
-
-    print_separator()
-    print('DATAFRAME:')
-    print(response_df)
-
-    print_separator()
-    print('GET POST BY ID:')
-    print(pd.DataFrame([json_api.get_post_by_id(1).json()]))
-
-    print_separator()
-    print('GET POSTS BY USER ID:')
-    print(pd.DataFrame(json_api.get_posts_by_user(1).json()))
-
-    print_separator()
-    print('CREATE POST:')
-    print(pd.DataFrame([json_api.create_post('New Post', 'This is a new post.', 1).json()]))
+    print(pd.DataFrame(raw_data[5]).dtypes)  # Display the data types of the 'users' DataFrame
+    
 
 
 if __name__ == '__main__':
